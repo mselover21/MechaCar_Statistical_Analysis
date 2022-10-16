d4bd28e2887e312464b66dcf8a1ef51110186ed4
Matthew Selover <mselover21@gmail.com>
2022-10-16 16:47:43 -0600

    Done with Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance +AWD
 
 # 6. Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance +AWD,data=mecha_mpg))
+
+# Deliverable 2
+
+# Read in the Suspension_Coil.csv
+sus_coil <- read.csv(file='Resources/Suspension_Coil.csv', check.names=F, stringsAsFactors=F)
+
+# 3.Write an RScript that creates a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+total_summary <- sus_coil %>% summarize(Mean_PSI=mean(PSI), Meadian_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coile=n(), .groups='keep')
+
+# 4. Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Meadian_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coile=n(), .groups='keep')
+
--- README.md
+++ README.md
@@ -12,3 +12,16 @@
 - The R-squared value is 0.7149. This means that the model predicts %71 of the miles per gallon. This also means that the model is effective in predicting miles per gallon.
 
 ![Summary_Deliverable_1.png](https://github.com/mselover21/MechaCar_Statistical_Analysis/blob/main/Images/Summary_Deliverable_1.png)
+
+#### Summary Statistics on Suspension Coils
+Total Summary
+
+![Total_Summary.png](https://github.com/mselover21/MechaCar_Statistical_Analysis/blob/main/Images/Total_Summary.png)
+
+Lot Summary
+
+![Lot_Summary.png](https://github.com/mselover21/MechaCar_Statistical_Analysis/blob/main/Images/Lot_Summary.png)
+
+Does the current manufacturing data meet this design specification for all manufacturing lots in total and each lot individually? Why or why not?
+
+- When looking at the total_summary the variance it 62.29356. This falls within the 100 PSI limit. When we break it down by lot the data tells a more interesting story. Lot 1 and Lot 2 are within the 100 PSI variance limit with 0.9795918 and 7.4693878 respectivly. Lot 3 has a variance of 170.2861224 and is way outside of the vanriance limit. 
